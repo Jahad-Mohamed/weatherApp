709e6b98deb18f5b9e4109e19540b5750222866a
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-05-13 08:23:36 +0100

    current location coords ✅

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # weatherApp
+
+This is a dynamic responsive Weather App that I created using HTML, Sass, and Vanila Javascript.
---
+++ dist/js/main.js
@@ -0,0 +1,43 @@
+import { setLocationObject } from "./dataFunctions.js";
+import CurrentLocation from "./CurrentLocation.js";
+import { addSpinner, displayError } from "./domFunction.js";
+
+const currentLoc = new CurrentLocation();
+
+const initApp = () => {
+  // add listeners
+  const geoButton = document.getElementById("getLocation");
+  geoButton.addEventListener("click", getGeoWeather);
+
+  // set up the
+  // load weather
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
+
+const getGeoWeather = (event) => {
+  if (event) {
+    if (event.type === "click") {
+      const mapIcon = document.querySelector(".fa-map-marker-alt");
+      addSpinner(mapIcon);
+    }
+  }
+  if (!navigator.geolocation) geoError();
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+};
+
+const geoError = (errObj) => {
+  const errMsg = errObj.message ? errObj.message : "Geolocation not supported";
+  displayError(errMsg, errMsg);
+};
+
+const geoSuccess = (position) => {
+  const myCoordsObj = {
+    lat: position.coords.latitude,
+    lon: position.coords.longitude,
+    name: `Lat:${position.coords.latitude} Long:${position.coords.longitude}`,
+  };
+
+  setLocationObject(currentLoc, myCoordsObj);
+  console.log(currentLoc);
+};
